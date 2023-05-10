9d5951e3ae49b5407481cee7f268a8a8ba8647d8
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-05-10 22:41:38 +0300

    new feature

--- main.sh
+++ main.sh
@@ -10,7 +10,24 @@ quote=$(grep -i "^${book_title}:" citates.txt)
 if [ -z "$quote" ]; then
     echo "Для данного произведения не найдено цитат"
     read -p "Хотите добавить цитату для этого произведения? (y/n): " choice
+    echo ""
 
+    if [ "$choice" == "y" ]; then
+        read -p "Введите цитату для произведения '$book_title': " new_quote
+        last_id=$(grep -oP '\[id:\K\d+' citates.txt | tail -1)
+        new_id=$((last_id + 1))
+        read -p "Введите номер главы для новой цитаты: " new_chapter
+        read -p "Введите номер страницы для новой цитаты: " new_page
+        read -p "Введите объяснение для новой цитаты: " new_explanation
+        echo "${book_title}: \"${new_quote}\" - Глава ${new_chapter}, страница ${new_page} [id:${new_id}]" >> citates.txt
+        echo "id:${new_id} ${new_explanation}" >> expl.txt
+        echo "Новая цитата с объяснением добавлена в базу данных"
+        exit 0
+    else
+        exit 1
+    fi
+fi
+    
 # Если нет, добавляем цитату
     if [ "$choice" == "y" ]; then
         read -p "Введите цитату для произведения '$book_title': " new_quote
