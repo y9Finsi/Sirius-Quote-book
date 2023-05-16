5db9aa3557bb522caa8c206cd7b0f6191fca7c5b
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-05-16 11:46:13 +0300

    добавлен while для main.sh

--- main.sh
+++ main.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
-# Получаем название произведения
-read -p "Введите название произведения: " book_title
+while true; do
+  # Получаем название произведения
+  echo ""
+  read -p "Введите название произведения (для выхода введите 'хватит'): " book_title
 
-# Получаем случайную цитату для данного произведения
-quote=$(grep -i "^${book_title}:" citates.txt)
-
-# Проверяем есть ли цитата
-if [ -z "$quote" ]; then
-    echo "Для данного произведения не найдено цитат"
-    read -p "Хотите добавить цитату для этого произведения? (y/n): " choice
-    echo ""
-
-    # Если нет, добавляем цитату
-    if [ "$choice" == "y" ]; then
-        read -p "Введите цитату для произведения '$book_title': " new_quote
-        last_id=$(grep -oP '\[id:\K\d+' citates.txt | tail -1)
-        new_id=$((last_id + 1))
-        read -p "Введите номер главы для новой цитаты: " new_chapter
-        read -p "Введите номер страницы для новой цитаты: " new_page
-        read -p "Введите объяснение для новой цитаты: " new_explanation
-        echo "${book_title}: \"${new_quote}\" - Глава ${new_chapter}, страница ${new_page} [id:${new_id}]" >> citates.txt
-        echo "id:${new_id} ${new_explanation}" >> expl.txt
-        echo ""
-        echo "Новая цитата с объяснением успешна добавлена в базу данных"
-        exit 0
-    else
-        exit 1
+  # Проверяем нужно ли заврешить программу
+  if [ "$book_title" == "хватит" ]; then
+    echo "программа завершена"
+    exit 0
+  fi
+  
+  # Получаем случайную цитату для данного произведения
+  quote=$(grep -i "^${book_title}:" citates.txt)
+  
+  # Проверяем есть ли цитата
+  if [ -z "$quote" ]; then
+      echo "Для данного произведения не найдено цитат"
+      read -p "Хотите добавить цитату для этого произведения? (y/n): " choice
+      echo ""
+  
+      # Если нет, и юзер соглсаен добавляем цитату
+      if [ "$choice" == "y" ]; then
+          read -p "Введите цитату для произведения '$book_title': " new_quote
+          last_id=$(grep -oP '\[id:\K\d+' citates.txt | tail -1)
+          new_id=$((last_id + 1))
+          read -p "Введите номер главы для новой цитаты: " new_chapter
+          read -p "Введите номер страницы для новой цитаты: " new_page
+          read -p "Введите объяснение для новой цитаты: " new_explanation
+          echo "${book_title}: \"${new_quote}\" - Глава ${new_chapter}, страница ${new_page} [id:${new_id}]" >> citates.txt
+          echo "id:${new_id} ${new_explanation}" >> expl.txt
+          echo ""
+          echo "Новая цитата с объяснением успешна добавлена в базу данных"
+      else
+          echo ""
+      fi
+  else
+      # Рандомно получаем цитату с помощью shuf
+      quote=$(echo "$quote" | shuf -n 1)
+      
+      # Извлекаем id цитаты
+      id=$(echo "$quote" | sed 's/.*\[id:\([0-9]\+\)\]/\1/')
+      
+      # Получаем объяснение для данной цитаты
+      explanation=$(grep -i "^id:${id}" expl.txt | sed 's/id:[0-9]\+//g')
+      
+      # Выводим цитату и объяснение
+      echo ""
+      echo "Цитата из ${quote/\[*\]/}"
+      echo ""
+      echo "Объяснение: $explanation"
     fi
-fi
-
-# Рандомно получаем цитату с помощью shuf
-quote=$(echo "$quote" | shuf -n 1)
-
-# Извлекаем id цитаты
-id=$(echo "$quote" | sed 's/.*\[id:\([0-9]\+\)\]/\1/')
-
-# Получаем объяснение для данной цитаты
-explanation=$(grep -i "^id:${id}" expl.txt | sed 's/id:[0-9]\+//g')
-
-# Выводим цитату и объяснение
-echo ""
-echo "Цитата из ${quote/\[*\]/}"
-echo ""
-echo "Объяснение: $explanation"
+done
